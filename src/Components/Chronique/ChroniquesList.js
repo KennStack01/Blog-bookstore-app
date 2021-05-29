d24fe0906e9093be64d4a868c831337012f5b9d6
Kenn Stack <kibadikennedy@gmail.com>
2021-05-30 04:44:52 +0800

    Initial Commit

--- src/Components/Chronique/ChroniquesList.js
+++ src/Components/Chronique/ChroniquesList.js
@@ -4,6 +4,7 @@ import { graphql, useStaticQuery } from 'gatsby'
 const ChroniquesList = () => {
 
     const data = useStaticQuery(graphql`
+    query {
         allContentfulChronique(sort: {fields: image___createdAt, order: DESC}) {
             edges {
                 node {
@@ -20,15 +21,35 @@ const ChroniquesList = () => {
                 }
             }
         }
+    }
     `)
+
+    const allChroniques = data.allContentfulChronique.edges.map(({node}) => node)
 
     return (
         <div>
             <div>
-                {/* Picture */}
-                {/* Title */}
-                {/* Description */}
+
                 {/* Buttons */}
+                 <div className="flex flex-col md:grid md:grid-cols-3">
+                    { allChroniques.map( (chronique) => (
+                        <section className="flex flex-col bg-white rounded-lg shadow md:h-80 h-72 w-72 m-auto my-4">
+                            {/* Picture */}
+                            <img
+                                className="w-full h-40 md:h-48 object-cover object-center rounded-t-lg"
+                                src={chronique.image.file.url}
+                                alt={chronique.image.description}
+                                />
+                            {/* Title */}
+                            <div className="flex-grow m-auto text-mirage-500 text-center mx-2">
+                                <div>
+                                    <h1 className="text-lg font-semibold font-sans"> {chronique.titreDeLaChronique} </h1>
+                                </div>
+                            </div>
+                            {/* Description */}
+                        </section>
+                    )) }
+                </div>
             </div>
         </div>
     )

66a4eb8fab74475994eaac1c770b2616317ae4a4
Kenn Stack <kibadikennedy@gmail.com>
2021-06-01 03:39:43 +0800

    updated component ChroniquesList

--- src/Components/Chronique/ChroniquesList.js
+++ src/Components/Chronique/ChroniquesList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
+import { HiOutlineShare } from 'react-icons/hi'
 
 const ChroniquesList = () => {
 
@@ -30,10 +31,10 @@ const ChroniquesList = () => {
         <div>
             <div>
 
-                {/* Buttons */}
+
                  <div className="flex flex-col md:grid md:grid-cols-3">
                     { allChroniques.map( (chronique) => (
-                        <section className="flex flex-col bg-white rounded-lg shadow md:h-80 h-72 w-72 m-auto my-4">
+                        <section className="flex flex-col bg-white rounded-lg shadow w-72 my-4 pb-3 mx-10" key={chronique.titreDeLaChronique}>
                             {/* Picture */}
                             <img
                                 className="w-full h-40 md:h-48 object-cover object-center rounded-t-lg"
@@ -43,10 +44,23 @@ const ChroniquesList = () => {
                             {/* Title */}
                             <div className="flex-grow m-auto text-mirage-500 text-center mx-2">
                                 <div>
-                                    <h1 className="text-md font-regular font-medium"> {chronique.titreDeLaChronique} </h1>
+                                    <h1 className="text-md font-normal mt-1"> {chronique.titreDeLaChronique} </h1>
                                 </div>
                             </div>
                             {/* Description */}
+                            <p className="text-xs font-extralight text-justify m-2 my-3">
+                                {/* {console.log(JSON.parse(chronique.descriptionDeLaChronique.raw))} */}
+                                {JSON.parse(chronique.descriptionDeLaChronique.raw).content[0].content[0].value.substring(0, 130) + '...'}
+                            </p>
+
+                            <div className="flex flex-row justify-end mt-2">
+                                <button className="px-3 text-white bg-mirage-500">
+                                    Lire
+                                </button>
+                                <div className="text-2xl mx-2 cursor-pointer">
+                                    <HiOutlineShare/>
+                                </div>
+                            </div>
                         </section>
                     )) }
                 </div>
---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NotFound

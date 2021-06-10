96b2761bcbf25c1e2205773459d97900c68345e6
Kenn Stack <kibadikennedy@gmail.com>
2021-06-10 19:25:20 +0800

    updated Seo Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from '../Components/Seo'
 export default function Home() {
   return (
     <div>
-      <SEO title="Les Chroniques" description="Evadez-vous à travers les Meilleures Chroniques." />
+      <SEO title="Accueil | Les Chroniques" description="Evadez-vous à travers les Meilleures Chroniques." />
       <Layout>
         <div className="mt-6 mb-4 text-mirage-500">
           <h1 className="text-2xl font-bold">Vers et Chroniques</h1>

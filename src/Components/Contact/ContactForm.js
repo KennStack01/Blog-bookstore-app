01bcedc4597f717b0718f99bf841e55e56898e5e
Kenn Stack <kibadikennedy@gmail.com>
2021-06-12 01:13:53 +0800

    added form styles and links

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,12 +1,13 @@
 import React from 'react'
 // import { navigate } from 'gatsby'
-import { Link } from 'gatsby'
+
 
 // We're using a library for sending email message from our HTML form: https://formsubmit.co/
 
 const ContactForm = () => {
     return (
         <div>
+
             <div className="mb-5">
                 📩 <span className="underline font-sans font-semibold text-mirage-500"> Message Direct ici </span> 👇 <br/>   
             </div>
@@ -49,7 +50,7 @@ const ContactForm = () => {
                         <textarea name="message" className="text-mirage-500 font-sans p-2 rounded border border-mirage-500 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent" required></textarea>
                     </label>
                 </p>
-                <button type="submit" className="bg-mirage-500 hover:bg-gray-300 py-2 w-36 rounded mb-5 font-sans">
+                <button type="submit" className="bg-mirage-500 hover:bg-mirage-700 py-2 w-36 rounded mb-5 font-sans">
                     <div className="flex flex-row justify-center text-mirage-500 font-semibold "> 
                         <h1 className="mr-1 mt-0 text-lg font-semibold text-white"> Envoyer </h1> 
                         <div className="text-3xl md:text-xl mb-2 animate-bounce w-3 h-3"> ✨ </div>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,13 @@ import Layout from '../Components/Layout'
 import ContactForm from '../Components/Contact/ContactForm'
 import SEO from '../Components/Seo'
 
+import { 
+    IoLogoFacebook,
+    IoLogoTwitter,
+    IoLogoInstagram
+} from 'react-icons/io5'
+
+import { ImWhatsapp } from 'react-icons/im'
 
 const contact = () => {
     return (
@@ -11,20 +18,39 @@ const contact = () => {
             <SEO title="Qui sommes-nous?" description="Découvrir l'Equipe de Vers et Chroniques" />
             <Layout>
                 <div className="mt-6 mb-4 text-mirage-500">
-                    <h1 className="text-2xl font-bold">Contact Direct</h1>
-                    <p className="text-md font-medium text-justify md:mx-10 mx-3 my-5">
-                        Vers et Chroniques est une réunion de passionnés de la littérature narrative et poétique qui associent 
-                        leurs énergies et savoirs pour vous donner des idées de lecture du monde francophone, faire découvrir 
-                        les auteurs et leurs pensées et vous offrir des livres à lire gratuitement ou à acheter. <br />
-                        <span className="">
-                            Peniel KATOMBE en est l’administrateur.
-                        </span>
-
-                    </p>
+                    <div className="mb-5">
+                        📩 <span className="underline font-sans font-semibold text-mirage-500"> Retrouvez-nous sur: </span> 👇 <br/>   
+                    </div>
+                    <div className="grid grid-cols-2 place-items-center text-xl md:text-2xl mt-2">
+                        <div className="cursor-pointer my-2">
+                            <a href="https://www.facebook.com/profile.php?id=100063121575005" target="__blank" className="flex flex-row">
+                                <IoLogoFacebook/>
+                                <p className="text-sm md:text-lg text-mirage-500"> Facebook </p>
+                            </a>
+                        </div>
+                        <div className="cursor-pointer my-2">
+                            <a href="https://twitter.com/PenielKatombe?s=09" target="__blank" className="flex flex-row">
+                                <IoLogoTwitter/>
+                                <p className="text-sm md:text-lg text-mirage-500"> Twitter </p>
+                            </a>
+                        </div>
+                        <div className="cursor-pointer my-2">
+                            <a href="https://www.instagram.com/invites/contact/?i=123d70ua56cjk&utm_content=oizab8" target="__blank" className="flex flex-row">
+                                <IoLogoInstagram/>
+                                <p className="text-sm md:text-lg text-mirage-500 "> Instagram </p>
+                            </a>
+                        </div>
+                        <div className="cursor-pointer my-2">
+                            <a href="https://wa.me/message/DWICK6VGS7TOI1" target="__blank" className="flex flex-row">
+                                <ImWhatsapp/>
+                                <p className="text-sm md:text-lg text-mirage-500"> WhatsApp </p>
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
                 <ContactForm/>
-                
+
                 <button className="bg-mirage-500 mx-auto mt-10 p-3 rounded-md font-semibold text-white"> 
                     <Link to="/">
                         Page d'Accueil <span role="img">🏡</span> 

3015f6b5ecba38d2bd1ce312e718e09f5dfb500a
Kenn Stack <kibadikennedy@gmail.com>
2021-07-13 00:07:33 +0800

    configurating my query

--- src/Components/Chronique/TemplateForChronique.js
+++ src/Components/Chronique/TemplateForChronique.js
@@ -1,56 +1,18 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
-import { IoArrowBackSharp } from 'react-icons/io5'
-import Bodytext from './BodyText'
 
-const TemplateForChronique = ({data}) => {
+// import Bodytext from './BodyText'
+
+const TemplateForChronique = () => {
 
     return (
         <div>
             <div>
-                <div>
-                    {/* Add Previous page link path: <Link to=".."></Link> */}
-                    <Link to='/' className="absolute z-50 bg-white text-3xl p-2 rounded-full ml-4 mt-4 shadow-md">
-                        <IoArrowBackSharp/>
-                    </Link>
-                </div>
-                <img 
-                    src={`${data.datoCmsChronique.imageDeLaChronique.url}`} 
-                    alt="" 
-                    // className="w-full h-48 md:h-60 object-cover object-center mt-6"
-                    className="object-cover relative object-center h-60 w-full"
-                />
-                <div className="flex flex-col">
-                    <h1 className="text-lg text-center font-semibold mx-3 my-4 md:mx-auto"> {data.datoCmsChronique.leTitreDeLaChronique} </h1>
-                    <div className="flex flex-row justify-between mx-3 mt-4 ">
-                        <p className="text-xs text-gray-700 md:mr-5">Auteur: <span className="font-semibold"> {data.datoCmsChronique.auteurDeLaChronique} </span> </p>
-                        <p className="text-xs text-gray-700 md:ml-5">{data.datoCmsChronique.dateDePublicationDeLaChronique} </p>
-                    </div>
-                    <div className="divide-y divide-mirage-500 mx-2">
-                        <div className="bottom-0"></div>
-                        <div className="bottom-0"></div>
-                    </div>
-                </div>
-
-                <Bodytext/>
+                <h1> Hello, happy Coding😁</h1>
+                {/* Header for Chronique */}
+                {/* <Bodytext/> */}
             </div>
         </div>
     )
 }
 
 export default TemplateForChronique
-
-
-export const query = graphql`
-    query ($slug: String!) {
-        datoCmsChronique(slug: {eq: $slug}) {
-            imageDeLaChronique {
-                url
-            }
-            leTitreDeLaChronique
-            auteurDeLaChronique
-            dateDePublicationDeLaChronique(formatString: "DD MMMM YYYY", locale: "fr")
-            slug
-        }
-    }
-`

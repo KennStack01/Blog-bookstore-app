6e329caf14fa7a82c400ba37b9a117f9cd907cd8
Kenn Stack <kibadikennedy@gmail.com>
2021-07-13 03:26:01 +0800

    still improving static query

--- src/Components/Chronique/TemplateForChronique.js
+++ src/Components/Chronique/TemplateForChronique.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import HeaderForChronique from './HeaderForChronique'
+import { useStaticQuery, StaticQuery, graphql } from 'gatsby'
 // import Bodytext from './BodyText'
 
 const TemplateForChronique = () => {
 
-    return (
-        <div>
-            <div>
-                {/* Header for Chronique */}
-                <HeaderForChronique />
-                {/* <Bodytext/> */}
-            </div>
-        </div>
+    return(
+        <StaticQuery
+            query={graphql`
+                query {
+                    datoCmsChronique{
+                        imageDeLaChronique {
+                            url
+                        }
+                        leTitreDeLaChronique
+                        auteurDeLaChronique
+                        dateDePublicationDeLaChronique(formatString: "DD MMMM YYYY", locale: "fr")
+                        slug
+                    }
+                }
+            `}
+            render={data => (
+                <HeaderForChronique data={data} />
+            )}
+        />
     )
+
+    // return (
+    //     <div>
+    //         <div>
+    //             {/* Header for Chronique */}
+    //             <HeaderForChronique data={data} />
+    //             {/* <Bodytext/> */}
+    //         </div>
+    //     </div>
+    // )
 }
 
 export default TemplateForChronique

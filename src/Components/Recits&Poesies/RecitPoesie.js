b3cef7ce7c0e72550b9477e41553d0597382530b
Kenn Stack <kibadikennedy@gmail.com>
2021-08-21 18:05:49 +0800

    redesigning the recit page

--- src/Components/Recits&Poesies/RecitPoesie.js
+++ src/Components/Recits&Poesies/RecitPoesie.js
@@ -31,24 +31,37 @@ const RecitPoesie = () => {
         <div>
             <div className="flex flex-col lg:grid lg:grid-cols-2">
                 { allRecits.map( (recit) => (
-                        <section className={`flex flex-row bg-white  w-auto md:w-90 rounded-l-lg border-r-4 ${borderColor[Math.floor(Math.random() * borderColor.length)]} shadow my-4 mx-2 transform transition duration-500 hover:scale-105`} key={recit.titreDuRecit}>
-                            {/* Picture */}
-                            <img
-                                className="w-32 md:w-48 md:h-32 object-cover object-center rounded-l-lg"
-                                src={recit.imageDuRecit.url}
-                                alt=""
-                                />
+                        <section className={`flex flex-row justify-between bg-white  w-auto md:w-90 rounded-l-lg border-r-4 ${borderColor[Math.floor(Math.random() * borderColor.length)]} shadow my-4 mx-2 transform transition duration-500 hover:scale-105`} key={recit.titreDuRecit}>
+                            <div className="flex flex-row justify-between">
+                                {/* Picture */}
+                                <Link to={`/recits/${recit.slug}`}>
+                                    <img
+                                        className="w-32 md:w-48 md:h-32 object-cover object-center rounded-l-lg"
+                                        src={recit.imageDuRecit.url}
+                                        alt=""
+                                        />
+                                </Link>
+                                    <div className="flex-grow m-auto text-mirage-500 text-center mx-2">
+                                        <Link to={`/recits/${recit.slug}`}>
+                                            <h1 className="text-xs md:text-lg font-semibold mt-1 hover:text-yellow-600 hover:underline"> {recit.titreDuRecit} </h1>
+                                        </Link>
+                                        {/* Description */}
+                                        <p className="text-xs font-normal text-justify m-2 my-1">
+                                            {`${recit.descriptionDuRecit.substring(0, 155)}...`}
+                                        </p>
+                                    </div>
+                            </div>
                             {/* Title */}
-                            <div className="flex flex-col justify-between">
-                                <div className="flex-grow m-auto text-mirage-500 text-center mx-2">
+                            <div className="my-auto md:mt-16">
+                                {/* <div className="flex-grow m-auto text-mirage-500 text-center mx-2">
                                     <Link to={`/recits/${recit.slug}`}>
                                         <h1 className="text-xs md:text-lg font-semibold mt-1 hover:text-yellow-600 hover:underline"> {recit.titreDuRecit} </h1>
                                     </Link>
-                                </div>
+                                </div> */}
                                 {/* Description */}
-                                <p className="text-xs font-normal text-justify m-2 my-1">
+                                {/* <p className="text-xs font-normal text-justify m-2 my-1">
                                     {`${recit.descriptionDuRecit.substring(0, 155)}...`}
-                                </p>
+                                </p> */}
 
                                 <div className="flex flex-row justify-end text-sm my-2 mr-5">
                                     <Link to={`/recits/${recit.slug}`}>
